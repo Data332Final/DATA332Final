ea873d2735266ab9124e77d28c8b746d6d8bfaaf
leandrag20 <leandragottschalk20@augustana.edu>
2023-05-09 20:19:03 -0500

    Update FinalDataShootings.R

--- FinalDataShootings.R
+++ FinalDataShootings.R
@@ -23,15 +23,20 @@ column_classes <- c("numeric", "numeric", "numeric", "character", "character")
 
 census <- read.csv('nyc_census_tracts.csv')
 censusBlock <- read.csv('census_block_loc.csv', stringsAsFactors = FALSE, colClasses = column_classes)
+shootingData <- read.csv('NYPD_Shooting_Incident_Data__Historic_.csv')
 
 censusBlock$BlockCode <- as.character(censusBlock$BlockCode)
 censusBlock$BlockCode <- substr(censusBlock$BlockCode, 1, nchar(censusBlock$BlockCode) - 4)
 names(censusBlock)[names(censusBlock) == "BlockCode"] <- "CensusTract"
+shootingData <- shootingData[complete.cases(shootingData), ]
 
 
 censusBlock <- subset(censusBlock, State == "NY")
 mergedData <- merge(census, censusBlock, by = "CensusTract", all.x = TRUE)
 mergedData <- mergedData[complete.cases(mergedData), ]
+mergedData <- merge(shootingData, mergedData, by = c("Longitude"), all.x = TRUE)
+
+
 
 
 
