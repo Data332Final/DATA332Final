ec911031a459ac3ba45732fbe015172ec310ef24
The Tyrants <127907132+H3LLPRINCE@users.noreply.github.com>
2023-05-15 12:31:35 -0500

    Add files via upload

---
+++ race proportion of shooting victims by borough.R
@@ -0,0 +1,37 @@
+# Install required packages if not already installed
+install.packages(c("ggplot2", "tidyverse", "dplyr"))
+
+# Load the required libraries
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
+# Set the working directory
+setwd("C:/Users/Surface Laptop 3/Desktop/data final projecct")
+
+# Read the census_block_loc.csv file
+df <- read.csv('census_block_loc.csv')
+
+# Read the nyc_census_tracts.csv file
+nyc_census_tracts <- read.csv("nyc_census_tracts.csv")
+
+# Read the NYPD_Shooting_Incident_Data__Historic_.csv file
+nypd_shooting_data <- read.csv("NYPD_Shooting_Incident_Data__Historic_.csv")
+str(df)
+str(nyc_census_tracts)
+str(nypd_shooting_data)
+
+
+
+
+library(ggplot2)
+
+# Filter the data to exclude missing or unknown race values
+filtered_data <- nypd_shooting_data[!is.na(nypd_shooting_data$VIC_RACE) & nypd_shooting_data$VIC_RACE != "", ]
+
+# Create a bar plot showing race proportion by borough
+ggplot(filtered_data, aes(x = BORO, fill = VIC_RACE)) +
+  geom_bar(position = "fill") +
+  labs(x = "Borough", y = "Proportion", fill = "Race") +
+  ggtitle("Race Proportion of Shooting Victims by Borough")
+
